94465fd4c0b1a239c5ef4df3c028652d839f14de
Geoff Phillips <geoffreymartinphillips@gmail.com>
2018-10-14 17:43:56 -0700

    Add example route and data helper

---
+++ server/lib/data-helpers.js
@@ -0,0 +1,15 @@
+export default function makeDataHelpers(db) {
+  return {
+
+    // Saves a tweet to `db`
+    saveTweet: function(newTweet, callback) {
+      db.collection("tweets").insertOne(newTweet, callback);
+    },
+
+    // Get all tweets in `db`, sorted by newest first
+    getTweets: function(callback) {
+      db.collection("tweets").find().toArray(callback);
+    }
+
+  };
+}
